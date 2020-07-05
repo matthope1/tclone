eed89ba83e6b2712ea343efb7771c0b8d4982e88
Matthew <matt-hope@hotmail.com>
2020-07-04 23:08:57 -0400

    added styles

--- application.py
+++ application.py
@@ -5,16 +5,34 @@ from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
-
 from helpers import apology, login_required
-
-
 from flask import Flask
+
 app = Flask(__name__)
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+# Custom filter
+app.jinja_env.filters["usd"] = usd
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app))
+
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return render_template("index.html") 
 
 def errorhandler(e):
     """Handle error"""
