15b22259c95b988a775775e5cacccf982844dd5c
Matthew <matt-hope@hotmail.com>
2020-07-09 20:04:09 -0400

    added apology template and configured database

--- application.py
+++ application.py
@@ -1,5 +1,4 @@
 import os
-
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
 from flask_session import Session
 from tempfile import mkdtemp
@@ -7,6 +6,7 @@ from werkzeug.exceptions import default_exceptions, HTTPException, InternalServe
 from werkzeug.security import check_password_hash, generate_password_hash
 from helpers import apology, login_required
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 # Ensure templates are auto-reloaded
@@ -20,15 +20,14 @@ def after_request(response):
     response.headers["Pragma"] = "no-cache"
     return response
 
-# Custom filter
-app.jinja_env.filters["usd"] = usd
-
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
-Session(app))
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///tclone.db'
+Session(app)
 
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
@@ -39,7 +38,6 @@ def errorhandler(e):
     if not isinstance(e, HTTPException):
         e = InternalServerError()
     return apology(e.name, e.code)
-
 
 # Listen for errors
 for code in default_exceptions:
