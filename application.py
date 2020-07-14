574e0c2bd5fa11fadedb322c40a5386af1ad5ade
Matthew <matt-hope@hotmail.com>
2020-07-14 14:06:03 -0400

    completed register page

--- application.py
+++ application.py
@@ -1,3 +1,4 @@
+import sqlite3 as lite
 import os
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
 from flask_session import Session
@@ -6,7 +7,8 @@ from werkzeug.exceptions import default_exceptions, HTTPException, InternalServe
 from werkzeug.security import check_password_hash, generate_password_hash
 from helpers import apology, login_required
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+
+#from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 # Ensure templates are auto-reloaded
@@ -24,30 +26,91 @@ def after_request(response):
 app.config["SESSION_FILE_DIR"] = mkdtemp()
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
-app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///tclone.db'
+#app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///tclone.db'
 Session(app)
 
-db = SQLAlchemy(app)
+# db = SQLAlchemy(app)
+
+con = lite.connect('tclone.db')
+
+with con:
+
+    cur = con.cursor()
+
+    username = "testUser"
+    password = generate_password_hash("testPass")
+
+    # cur.execute("INSERT INTO users (username, hash) values (?,?)", (username, password))
+    # cur.execute("INSERT INTO users VALUES (?, ?)", username, password)
+
+    t = (username, ) #needs to provide a touple of values for the query
+
+    cur.execute("SELECT * FROM users WHERE username=?", t)
+
+    print(cur.fetchone())
+    # for row in username_query:
+        # print(row)
+
+
+    con.commit()
+# con.close()
+
+print("insert complete")
 
 @app.route('/')
+@login_required
 def index():
     return render_template("index.html") 
 
+@app.route('/login', methods=["GET", "POST"])
+def login():
+
+    #user reached login via post
+    if request.method == "POST": 
+        print("login POST todo:")
+    #user reached login via get  
+    else:
+        print("login GET todo: ")
+
 @app.route('/register', methods=["GET","POST"])
 def register():
-    
+
+    #user reached this route with the post method  
     if request.method == "POST":
         
         username = request.form.get("username")
         password = request.form.get("password")
         confirmation = request.form.get("confirmation")
+        
+        if not username:
+            return apology("Must enter a username")
 
-        print(username)
-        print(password)
-        print(confirmation)
+        #connect to database
+        con = lite.connect('tclone.db')
 
+        with con:
+            cur = con.cursor()
+            q = (username, )
+            username_query = cur.execute("SELECT * from users WHERE username=?", q)
+            #print(cur.fetchone())
+
+            #if the query doesn't return 0 rows then the username is not available 
+            if cur.fetchone() != None:
+                return apology("Username taken")
+
+        if not password:
+            return apology("Must enter a password")
+            
+        if password != confirmation:
+            return apology("Passwords must match")
+
+        hashed_pass = generate_password_hash(password)
+        cur.execute("INSERT INTO users (username, hash) values (?,?)", (username, hashed_pass))
+
+        flash("Registration successful")      
         return render_template("login.html")
 
+    #user reached this route with the get method
     else:
         return render_template("register.html")
 
